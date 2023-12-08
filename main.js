8f02bdfe3af6e4609beabba9af03fa757dc1b925
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2023-12-08 01:53:18 +0100

    first stable v

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var messages = [
+let messages = [
   /* Array of 300 messages*/
   " You are my motivation, my muse, the one person I would never want to lose at any cost. I love you with all my heart because you have made life worth living every day.",
 
@@ -15,8 +15,6 @@ var messages = [
 
   " Every hour, every minute, every second that passes I love you more and more. I could spend a thousand lifetimes with you and still want more because your love is like a bottomless well. I can never get enough of you my love.",
 
-  " I want you, I need you, I yearn for you, I lust for you, and most importantly I love you. You are the other half of my heart and the better half of my soul. You make me whole.",
-
   " My dearest, I don’t know where I would be without your love. You bring out the best in me and give me strength when I am feeling low. You are everything that is right in this world. I love you.",
   "You are my shining star, my beautiful angel, and my amazing queen. You are the most wonderful person that I have ever met and the best thing that has ever happened to me. I cherish you always",
 
@@ -32,17 +30,11 @@ var messages = [
 
   "When you smiled at me for the first time, I felt like I was flying higher than any bird. When you kissed me for the first time, I felt like I was drowning in a sea of pure bliss. When you embraced me for the first time, I felt truly blessed to have your love",
 
-  "I want to hold you forever. I want to feel your touch deep within my soul. I want to be with you always. I want to smell your hair, feel your skin, and let your scent consume me. For without you, I am nothing",
-
   "You are my whole world. Those three words are not enough to describe my love for you. There just aren’t enough words to describe how wonderful you make me feel",
 
   "I feel your love in each and every touch. Every time I see you, I feel warm inside, like the sun shining down upon me. I love you with all of my heart and soul",
 
-  "You are my soul mate, my one and only. I want to be with you for all of eternity. I will love you until the end of time",
-
   "Every time I think about you my heart skips more than a few beats. Your eyes, your smile, your hair, everything is beautiful to me. I want to hold you close each and every night when we go to sleep",
-
-  "You are my heart, you are my soul, you are everything to me. I love you",
 
   "I want to stay with you forever and always. I want to hold your hand in the street. I want to sleep with you every night. I want to feel your lips on mine. You are the one for me. I love you",
 
@@ -82,6 +74,123 @@ var messages = [
   "You are my special angel sent from up above. I love you",
 ];
 
+let short_messages = [
+  "You light up my world with your presence.",
+  "Our friendship is a treasure I hold close to my heart.",
+  "In the story of my life, you're the most beautiful chapter.",
+  "Your smile is my daily dose of happiness.",
+  "Being with you is like a beautiful dream that never ends.",
+  "You're not just a friend; you're my confidant and partner in crime.",
+  "Your laughter is the melody of my happiest moments.",
+  "With you, every day is an adventure filled with joy and laughter.",
+  "Thank you for being my rock, my support, and my best friend.",
+  "Your kindness and warmth make the world a better place.",
+  "I cherish the countless memories we've created together.",
+  "Your friendship is the greatest gift I've ever received.",
+  "In a garden full of flowers, you're the most exquisite bloom.",
+  "With you, even the simplest moments become extraordinary.",
+  "Our friendship is a bond that grows stronger with each passing day.",
+  "You are the sunshine that brightens my darkest days.",
+  "In the journey of life, having you by my side makes it worthwhile.",
+  "You're the missing piece that completes the puzzle of my happiness.",
+  "Your presence adds color to the canvas of my life.",
+  "With you, every moment is a beautiful memory in the making.",
+  "You make ordinary days extraordinary just by being in them.",
+  "Our friendship is a symphony of love and understanding.",
+  "You're not just a friend; you're family I chose for myself.",
+  "Every day spent with you is a day well-lived and full of love.",
+  "Your strength and resilience inspire me to be a better person.",
+  "You're my confidant, my partner in crime, and my greatest support.",
+  "Our friendship is the anchor that keeps me grounded.",
+  "With you, every hurdle becomes a stepping stone to success.",
+  "You're the reason my heart smiles every day.",
+  "In the book of my life, you have the most highlighted pages.",
+  "Your friendship is the greatest blessing I've ever received.",
+  "You have a way of making everything brighter with your presence.",
+  "Your friendship is the glue that holds my life together.",
+  "Thank you for being the constant in my ever-changing life.",
+  "You're my partner in silliness, my co-conspirator in laughter.",
+  "Our friendship is the kind that movies are made about.",
+  "You're the melody in the soundtrack of my happiest memories.",
+  "With you, even the mundane becomes a magical adventure.",
+  "Your wisdom and guidance are my beacons in the journey of life.",
+  "You're the anchor that keeps me steady in the storms of life.",
+  "Thank you for being the friend who walks beside me through it all.",
+  "Your presence makes the world a brighter and happier place.",
+  "You're the friend I can always count on, no matter what.",
+  "Our friendship is a beautiful tapestry woven with threads of love.",
+  "With you, every moment is a celebration of friendship.",
+  "Your friendship is the compass that guides me in the right direction.",
+  "You're not just a friend; you're the family I choose for myself.",
+  "Every day with you is a new page in the adventure of our friendship.",
+  "Thank you for being the friend who knows me better than I know myself.",
+  "Your laughter is the sweetest melody that plays in my heart.",
+  "In the dance of life, you're my favorite partner.",
+  "You're the sunshine that brightens even the gloomiest days.",
+  "Our friendship is a journey I cherish and look forward to every day.",
+  "Your kindness is a beacon of light in the darkness of the world.",
+  "You're the friend who turns ordinary moments into extraordinary memories.",
+  "With you, every challenge becomes an opportunity for growth.",
+  "Your friendship is the most precious gift I've ever received.",
+  "Thank you for being the friend who shares both my joys and sorrows.",
+  "You're the missing piece that completes the puzzle of my happiness.",
+  "Your love and support are the pillars of our beautiful friendship.",
+  "You're the friend who makes my world a better and brighter place.",
+  "In the tapestry of life, you're the thread that adds color and vibrancy.",
+  "With you, every day is an adventure filled with laughter and joy.",
+  "Your friendship is the anchor that keeps me grounded in the storm.",
+  "You're the reason my heart smiles, even on the toughest days.",
+  "Our friendship is the kind that grows stronger with each passing day.",
+  "Your presence is a gift that makes every moment more special.",
+  "Thank you for being the friend who makes life's journey worthwhile.",
+  "You're the friend who turns ordinary days into extraordinary moments.",
+  "Your wisdom and understanding are the treasures of our friendship.",
+  "With you, every moment becomes a cherished memory.",
+  "You're the sunshine that brightens the darkest corners of my life.",
+  "In the melody of life, your friendship is the sweetest tune.",
+  "Your laughter is the soundtrack of my happiest memories.",
+  "Thank you for being the friend who makes my heart sing with joy.",
+  "You're the friend who walks beside me through every twist and turn.",
+  "With you, every day is a new chapter in the book of our friendship.",
+  "Your friendship is the compass that guides me in the right direction.",
+  "You're the anchor that keeps me steady in the storms of life.",
+  "Thank you for being the friend who knows me inside out.",
+  "Your laughter is the sweetest melody that plays in my heart.",
+  "In the dance of life, you're my favorite partner.",
+  "You're the sunshine that brightens even the gloomiest days.",
+  "Our friendship is a journey I cherish and look forward to every day.",
+  "Your kindness is a beacon of light in the darkness of the world.",
+  "You're the friend who turns ordinary moments into extraordinary memories.",
+  "With you, every challenge becomes an opportunity for growth.",
+  "Your friendship is the most precious gift I've ever received.",
+  "Thank you for being the friend who shares both my joys and sorrows.",
+  "You're the missing piece that completes the puzzle of my happiness.",
+  "Your love and support are the pillars of our beautiful friendship.",
+  "You're the friend who makes my world a better and brighter place.",
+  "In the tapestry of life, you're the thread that adds color and vibrancy.",
+  "With you, every day is an adventure filled with laughter and joy.",
+  "Our friendship is the kind that grows stronger with each passing day.",
+  "Your presence is a gift that makes every moment more special.",
+  "Thank you for being the friend who makes life's journey worthwhile.",
+];
+
+function displayRandomMessageShort() {
+  // Get a random index from the messages array
+  var randomIndexShort = Math.floor(Math.random() * short_messages.length);
+
+  // Display the random message
+  document.getElementById("messageDisplayShort").innerHTML =
+    messages[randomIndexShort];
+}
+// Initial display
+displayRandomMessageShort();
+// Display a random message every 7 hours
+setInterval(displayRandomMessageShort, 25555500);
+
+//display random message when the body loads
+//document.body.addEventListener("load", displayRandomMessageShort);
+//document.body.addEventListener("load", displayRandomMessage);
+
 function displayRandomMessage() {
   // Get a random index from the messages array
   var randomIndex = Math.floor(Math.random() * messages.length);
@@ -89,12 +198,10 @@ function displayRandomMessage() {
   // Display the random message
   document.getElementById("messageDisplay").innerHTML = messages[randomIndex];
 }
-
-// Display a random message every 10 seconds
-setInterval(displayRandomMessage, 1000);
-
 // Initial display
 displayRandomMessage();
+// Display a random message every 1 day
+setInterval(displayRandomMessage, 86400000);
 
 // Uncomment the line below if you want to stop displaying messages after a certain time (e.g., 5 minutes)
 // setTimeout(() => clearInterval(messageInterval), 300000); // 300,000 milliseconds = 5 minutes
@@ -118,4 +225,8 @@ document.body.addEventListener("load", getJoke);
 btn.addEventListener("click", getJoke);
 getJoke();
 
+//light and dark mode
+function dark() {
+  document.body.classList.toggle("dark-mode");
+}
 //make background image her picture in ASCI
